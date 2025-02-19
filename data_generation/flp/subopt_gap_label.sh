870d4211f441c4273946eedcc149bcd49bc01298
hhhhuiyuan <huiyuan@princeton.edu>
2025-02-19 16:05:29 -0500

    clean up data

--- data_generation/flp/subopt_gap_label.sh
+++
@@ -1,8 +0,0 @@
-#run by $bash data/flp/subopt_gap_label.sh
-source activate Digress
-
-python -u data/flp/subopt_gap_label.py \
-  --subopt_file "/data/shared/huiyuan/flp/flp_train_subopt.txt" \
-  --opt_file "/data/shared/huiyuan/flp/flp_train_opt.txt" \
-  --new_file "/data/shared/huiyuan/flp/flp_train_subopt_gaplabel.txt" \
-  --task flp
--- data_generation/visualize.sh
+++
@@ -1,6 +0,0 @@
-#run by $bash data/visualize.sh
-source activate Digress
-
-python -u data/vis_data_subopt.py \
-  --subopt_file "./data/tsp20_rwd/tsp20_train.txt" \
-  --opt_file ".txt"
